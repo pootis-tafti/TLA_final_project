83883f3fc6cdc81cfad22aa2200d4c9a4ca8fffd
parsa sadeghi tafti <p.sadeghitafti@email.kntu.ac.ir>
2024-02-03 23:01:45 +0330

    init extra credit

---
+++ ir/ac/kntu/ExtraCredit/IFCFG.java
@@ -0,0 +1,16 @@
+package ir.ac.kntu.ExtraCredit;
+
+import ir.ac.kntu.MainProject.CFG;
+
+public class IFCFG extends CFG{
+
+    public IFCFG(String cfg) {
+        super(cfg);
+    }
+
+    @Override
+    public void parse(String cfg) {
+        // TODO Auto-generated method stub
+        super.parse(cfg);
+    }
+}
